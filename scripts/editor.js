a35acbb3d81890fc7c7dfb2a98259c9764dac698
Moxvallix Mox <moxvallix@gmail.com>
2024-04-11 01:02:00 +0930

    feat(editor): add new shade tool, refactor some tool config code

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,16 +1,31 @@
 import {CopperOre} from "./vendor/copper_ore/src/copper_ore.js"
 import {clamp, hexToRGB, sample, getRandomInt, shadeColor} from "./helpers.js"
 
-const NCRSEditorSettings = {
-  currentColor: {r: 255, b: 0, g: 0, a: 255},
-  blendPalette: [],
-  effects: {
+class NCRSEditorSettingsClass extends EventTarget {
+  currentColor = {r: 255, b: 0, g: 0, a: 255};
+  blendPalette = [];
+  effects = {
     camo: false,
     mirror: false,
-    blend: false
-  },
-  brushSize: 1
+    blend: false,
+    darken: false,
+    shadeOnce: false
+  };
+  brushSize = 1;
+  shadeStep = 1;
+
+  toggleEffect(effect) {
+    this.effects[effect] = !this.effects[effect];
+    this.dispatchEvent(new CustomEvent("effect", {detail: {effect: effect, value: this.effects[effect]}}));
+  }
+
+  setBrushSize(size) {
+    this.brushSize = size;
+    this.dispatchEvent(new CustomEvent("brushSize", {detail: {size: size}}));
+  }
 }
+
+const NCRSEditorSettings = new NCRSEditorSettingsClass;
 
 const NCRSEditor = new CopperOre({
   texture: '/mncs-mascot.png',
@@ -19,23 +34,19 @@ const NCRSEditor = new CopperOre({
   tools: {
       brush: UseBrush,
       bucket: UseBucket,
+      shade: UseShade,
       // eraser: UseEraser,
       // color_picker: UseColorPicker
   }
 });
+
+let shadeHistory = [];
 
 function ApplyBrush(intermediateTexture, point, color){
   const size = NCRSEditorSettings.brushSize;
   const square = size ** 2;
   const center = Math.floor(size / 2);
   for (let i = 0; i < square; i++) {
-    let useColor;
-    if (typeof color === "function") {
-      let newColor = color();
-      useColor = [newColor.r, newColor.g, newColor.b, newColor.a]
-    } else {
-      useColor = color;
-    }
     const row = i % size;
     const col = Math.floor(i / size);
 
@@ -44,6 +55,17 @@ function ApplyBrush(intermediateTexture, point, color){
 
     const pointX = clamp(point.x + offsetX, 0, NCRSEditor.IMAGE_WIDTH)
     const pointY = clamp(point.y + offsetY, 0, NCRSEditor.IMAGE_HEIGHT)
+
+    let useColor;
+    if (typeof color === "function") {
+      let newColor = color(intermediateTexture, {x: pointX, y: pointY});
+      if (!newColor) { return }
+      useColor = [newColor.r, newColor.g, newColor.b, newColor.a]
+    } else {
+      useColor = color;
+    }
+
+    if (!color) { return }
 
     intermediateTexture.ChangePixelAtArray({x: pointX, y: pointY}, useColor);
   }
@@ -65,6 +87,30 @@ function UseBucket(part, canvasTexture, pixel) {
     let newColor = getColor();
     return {r: newColor.r / 255, g: newColor.g / 255, b: newColor.b / 255, a: newColor.a / 255}
   });
+}
+
+function UseShade(_, canvasTexture, pixel) {
+  ApplyBrush(canvasTexture, pixel, shadePixel)
+}
+
+function shadePixel(texture, point) {
+  const color = texture.GetRGBA(point)
+  if (color.a <= 0) { return false }
+  if (NCRSEditorSettings.effects.shadeOnce) {
+    if (NCRSEditor.firstClick) { shadeHistory = [] }
+    const pointStr = `${point.x}:${point.y}`
+    if ( shadeHistory.includes(pointStr) ) { return false }
+    shadeHistory.push(pointStr)
+  }
+  const threeColor = texture.GetColorAt(point)
+  let hsl = {}
+  threeColor.getHSL(hsl)
+  const lightness = hsl.l + ((NCRSEditorSettings.shadeStep / 255) * (NCRSEditorSettings.effects.darken ? -1 : 1))
+  threeColor.setHSL(hsl.h, hsl.s, lightness)
+  let newColor = {}
+  threeColor.getRGB(newColor)
+  newColor.a = 255
+  return {r: newColor.r * 255, b: newColor.b * 255, g: newColor.g * 255, a: color.a}
 }
 
 function getColor() {
--- scripts/ui/tool-config.js
+++ scripts/ui/tool-config.js
@@ -1,36 +1,65 @@
+import { call } from "three/examples/jsm/nodes/Nodes.js"
 import {NCRSEditorSettings} from "../editor.js"
 
-function sizeInput(sizes) {
-  const sizeInput = document.createElement('fieldset')
-  sizeInput.classList.add('flex')
+function configToggle(title, name, data, callback) {
+  const configField = document.createElement('fieldset')
+  configField.classList.add('flex')
   
-  const sizeTitle = document.createElement('legend')
-  sizeTitle.innerText = "Size"
-  sizeTitle.classList.add('text-xs', 'text-gray-400')
-  sizeInput.append(sizeTitle)
+  const configTitle = document.createElement('legend')
+  configTitle.innerText = title
+  configTitle.classList.add('text-xs', 'text-gray-400')
+  configField.append(configTitle)
 
-  sizes.forEach(size => {
-    const sizeRadio = document.createElement('input')
-    sizeRadio.type = 'radio'
-    sizeRadio.innerText = size[0]
-    sizeRadio.setAttribute('name', 'size')
-    if (size == sizes.at(0)) {
-      sizeRadio.checked = true
-      sizeRadio.classList.add('rounded-bl-lg')
+  data.forEach(value => {
+    const radio = document.createElement('input')
+    radio.type = 'radio'
+    radio.innerText = value[0]
+    radio.setAttribute('name', name)
+    if (value == data.at(0)) {
+      radio.checked = true
+      radio.classList.add('rounded-bl-lg')
     }
-    if (size == sizes.at(-1)) {
-      sizeRadio.classList.add('rounded-br-lg')
+    if (value == data.at(-1)) {
+      radio.classList.add('rounded-br-lg')
     }
-    sizeRadio.setAttribute('data-size', size[1])
-    sizeRadio.setAttribute('title', `Set brush size of ${size[1]}.`)
-    sizeRadio.classList.add('btn-ncs', 'font-icon', 'appearance-none', 'select-none')
-    sizeRadio.addEventListener('click', event => {
-      NCRSEditorSettings.brushSize = Number(event.target.dataset.size)
-    })
-    sizeInput.append(sizeRadio)
+    radio.classList.add('btn-ncs', 'font-icon', 'appearance-none', 'select-none')
+
+    callback(value, radio)
+
+    configField.append(radio)
   });
 
+  return configField;
+}
+
+function sizeInput(name, sizes) {
+  const sizeInput = configToggle("Size", name, sizes, (value, radio) => {
+    radio.setAttribute('title', `Set brush size of ${value[1]}.`)
+    radio.setAttribute('data-size', value[1])
+    radio.addEventListener('click', event => {
+      NCRSEditorSettings.setBrushSize(Number(event.target.dataset.size))
+    })
+    NCRSEditorSettings.addEventListener("brushSize", event => {
+      console.log(event)
+      if (value[1] == event.detail.size) {
+        radio.checked = true
+      } else {
+        radio.checked = false
+      }
+    })
+  })
   return sizeInput;
+}
+
+function effectField(title = "Effects") {
+  const effects = document.createElement('fieldset')
+  effects.classList.add('flex', 'gap-1')
+
+  const effectsTitle = document.createElement('legend')
+  effectsTitle.innerText = title
+  effectsTitle.classList.add('text-xs', 'text-gray-400')
+  effects.append(effectsTitle)
+  return effects
 }
 
 function effectToggle(effect, icon, title = "") {
@@ -39,14 +68,17 @@ function effectToggle(effect, icon, title = "") {
   effectToggle.innerText = icon
   effectToggle.title = title
 
-  effectToggle.addEventListener('click', (event) => {
-    let eventState = NCRSEditorSettings.effects[effect]
-    if (eventState) {
-      event.target.classList.remove('btn-ncs-active')
+  effectToggle.addEventListener('click', () => {
+    NCRSEditorSettings.toggleEffect(effect);
+  })
+
+  NCRSEditorSettings.addEventListener("effect", event => {
+    if (event.detail.effect != effect) { return }
+    if (event.detail.value) {
+      effectToggle.classList.add('btn-ncs-active')
     } else {
-      event.target.classList.add('btn-ncs-active')
+      effectToggle.classList.remove('btn-ncs-active')
     }
-    NCRSEditorSettings.effects[effect] = !eventState
   })
 
   return effectToggle
@@ -60,15 +92,9 @@ function brushConfig() {
     ["\ue813", 3]
   ]
 
-  panel.append(sizeInput(brushSizes))
+  panel.append(sizeInput("brush-size", brushSizes))
 
-  const effects = document.createElement('fieldset')
-  effects.classList.add('flex', 'gap-1')
-
-  const effectsTitle = document.createElement('legend')
-  effectsTitle.innerText = "Effects"
-  effectsTitle.classList.add('text-xs', 'text-gray-400')
-  effects.append(effectsTitle)
+  const effects = effectField()
 
   effects.append(effectToggle("camo", "\ue821", "Toggle camo mode"))
   effects.append(effectToggle("blend", "\ue810", "Pick colors from the blend palette at random."))
@@ -78,6 +104,52 @@ function brushConfig() {
   panel.append(effects)
 }
 
+function bucketConfig() {
+  const panel = document.getElementById('bucket-config');
+
+  const effects = effectField()
+
+  effects.append(effectToggle("camo", "\ue821", "Toggle camo mode"))
+  effects.append(effectToggle("blend", "\ue810", "Pick colors from the blend palette at random."))
+  panel.append(effects)
+}
+
+function shadeConfig() {
+  const panel = document.getElementById('shade-config');
+  const subPanel = document.createElement('div');
+  subPanel.classList.add('flex', 'gap-2')
+  const brushSizes = [
+    ["\ue80e", 1],
+    ["\ue824", 2],
+    ["\ue813", 3]
+  ]
+  const stepSizes = [
+    ["1", 1],
+    ["3", 3],
+    ["5", 5]
+  ]
+
+  const effects = effectField("Settings")
+
+  effects.append(effectToggle("darken", "\ue827", "Darken mode."))
+  effects.append(effectToggle("shadeOnce", "\ue812", "Only shade a pixel once in a given stroke."))
+
+  subPanel.append(sizeInput("shade-size", brushSizes))
+  subPanel.append(
+    configToggle("Shade Steps", "shade-steps", stepSizes, (value, radio) => {
+      radio.setAttribute('title', `Set shade steps to ${value[1]}.`)
+      radio.setAttribute('data-step', value[1])
+      radio.addEventListener('click', event => {
+        NCRSEditorSettings.shadeStep = Number(event.target.dataset.step)
+      })
+    })
+  )
+  panel.append(subPanel)
+  panel.append(effects)
+}
+
 window.addEventListener('load', () => {
   brushConfig();
+  bucketConfig();
+  shadeConfig();
 })
